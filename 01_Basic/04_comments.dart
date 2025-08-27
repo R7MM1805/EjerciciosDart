3862fd48371c2957bde88d9ca3d0772228a1b05c
Ricardo Metzger <r7metzger@gmail.com>
2025-08-26 22:07:49 -0500

    feat: comentarios en dart

---
+++ 01_Basic/04_comments.dart
@@ -0,0 +1,18 @@
+void main(){
+  //Este es un comentario de una sola línea.
+  //Esta es una variable temporal de tipo int
+  int a = 10; //a es inicialmente 10
+
+  /*
+  //Este es un listado de nombres de personas
+  final people = ['Ricardo', 'Rosa', 'Álvaro'];
+  */
+
+}
+
+// Name: persona que deseo saludar
+// Message: Saludo que deseo hacer
+
+/// Función de saludar
+/// Recibe un [name] y se concatena con el [message]
+greeting(String name, String message){}
