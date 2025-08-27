a34ee5c5c3e261cc6efd82523061e15e30f742ee
Ricardo Metzger <r7metzger@gmail.com>
2025-08-26 22:46:39 -0500

    feat: operadores relacionales

--- 01_Basic/06_operators.dart
+++ 01_Basic/06_operators.dart
@@ -9,4 +9,22 @@ void main(){
   //Operadores condicionales
   String message = a > b ? 'A es mayor a b' : 'A es menor a b';
   print(message);
+
+  //Operadores relacionales
+  int x = 20;
+  int y = 30;
+  print(x > y);
+  print(x < y);
+  print(x >= y);
+  print(x <= y);
+  print(x == y);
+  print(x != y);
+
+  //Operadores de tipo
+  int i = 10;
+  String j = '10';
+  print( i is int );
+  print( j is int );
+  print( i is String);
+  print( j is String);
 }
