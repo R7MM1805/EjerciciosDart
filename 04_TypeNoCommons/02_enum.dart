ec4f1fe908b3f510bd8433c6e5a17d821ebae102
Ricardo Metzger <r7metzger@gmail.com>
2025-09-01 23:47:36 -0500

    enum en dart

---
+++ 04_TypeNoCommons/02_enum.dart
@@ -0,0 +1,21 @@
+void main(){
+  Audio volumen = Audio.alto;
+  switch (volumen) {
+    case Audio.bajo:
+    print( 'Volumen bajo' );
+      break;
+          case Audio.medio:
+    print( 'Volumen medio' );
+      break;
+          case Audio.alto:
+    print( 'Volumen alto' );
+      break;
+  }
+  
+}
+
+enum Audio {
+  bajo,
+  medio,
+  alto
+}
