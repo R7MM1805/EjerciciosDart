1835e9b4617e868e2c15357505de451438d06d82
Ricardo Metzger <r7metzger@gmail.com>
2025-10-01 23:28:16 -0500

    feat: streams en dart parte 3

--- 04_TypeNoCommons/07_stream.dart
+++ 04_TypeNoCommons/07_stream.dart
@@ -1,11 +1,20 @@
 import 'dart:async';
 
 void main(){
-  final StreamController streamController = StreamController();
+  final StreamController<String> streamController = StreamController.broadcast();
+
   streamController.stream.listen(
     (data) => print('Despegando... $data'),
     onError: (error) => print('Error: $error'),
-    cancelOnError: true
+    onDone: () => print('Mision Completa'),
+    cancelOnError: false
+  );
+
+  streamController.stream.listen(
+    (data) => print('Despegando 2... $data'),
+    onError: (error) => print('Error 2: $error'),
+    onDone: () => print('Mision Completa 2'),
+    cancelOnError: false
   );
 
   streamController.sink.add('Apollo 11');
@@ -13,6 +22,7 @@ void main(){
   streamController.sink.add('Apollo 13');
   streamController.sink.addError('Houston, tenemos un programa');
   streamController.sink.add('Apollo 14');
+  streamController.sink.add('Apollo 15');
 
   streamController.sink.close();
 
